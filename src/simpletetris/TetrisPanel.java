e486fdbaecb1e7921276c7a226709bb7673423f9
Gui Guy <jedandeganwang1999@gmail.com>
2018-08-21 15:18:17 -0500

    drawing stuff

--- src/simpletetris/TetrisPanel.java
+++ src/simpletetris/TetrisPanel.java
@@ -1,7 +1,9 @@
 package simpletetris;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
 
 /**
@@ -29,6 +31,21 @@ public class TetrisPanel extends JPanel implements Runnable {
     @Override
     public void paint(Graphics g) {
         Graphics2D g2D = (Graphics2D) g;
+        drawBackground(g2D, Color.WHITE);
+    }
+    
+    /**
+     * Fills the background with a solid color
+     * @param g2D the Graphics2D to draw with
+     * @param c the color to fill with
+     */
+    public void drawBackground(Graphics2D g2D, Color c) {
+        g2D.setColor(c);
+        g2D.fillRect(0, 0, getWidth(), getHeight());
+    }
+    
+    public void drawBackground(Graphics2D g2D, BufferedImage bi) {
+        g2D.drawImage(bi, 0, 0, getWidth(), getHeight(), null);
     }
 
     @Override
